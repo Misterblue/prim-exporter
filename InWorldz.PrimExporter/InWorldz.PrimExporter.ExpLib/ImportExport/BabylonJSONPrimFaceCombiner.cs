c86320fc855279d3154ca4a7020f5a42d22c5d86
David Daeschler <david.daeschler@gmail.com>
2016-10-12 11:41:09 -0500

    Complete retriangulation

--- InWorldz.PrimExporter/InWorldz.PrimExporter.ExpLib/ImportExport/BabylonJSONPrimFaceCombiner.cs
+++ InWorldz.PrimExporter/InWorldz.PrimExporter.ExpLib/ImportExport/BabylonJSONPrimFaceCombiner.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using InWorldz.PrimExporter.ExpLib.PrimFace;
+using OpenMetaverse;
 
 namespace InWorldz.PrimExporter.ExpLib.ImportExport
 {
@@ -31,24 +33,71 @@ namespace InWorldz.PrimExporter.ExpLib.ImportExport
 
             PrimFace.FaceData faceData = (PrimFace.FaceData)face.UserData;
 
-            //dump the vertices as they are
-            Vertices.AddRange(faceData.Vertices);
-            Normals.AddRange(faceData.Normals);
-            UVs.AddRange(faceData.TexCoords);
-                
-            //dump a material for the entire VIEWER FACE
-            Materials.Add(face.TextureFace);
-
+            List<Tuple<Vector3, Vector2, Vector3>> newVertsWithUvsAndNormals = new List<Tuple<Vector3, Vector2, Vector3>>();
             for (int i = 0; i < faceData.Indices.Length; i += 3)
             {
                 ushort a = (ushort)(faceData.Indices[i] + (verticesBase / 3));
                 ushort b = (ushort)(faceData.Indices[i + 1] + (verticesBase / 3));
                 ushort c = (ushort)(faceData.Indices[i + 2] + (verticesBase / 3));
 
-                Indices.Add(c);
-                Indices.Add(b);
-                Indices.Add(a);
+                //insert as C, B, A
+                //swap UVs between C and A
+                newVertsWithUvsAndNormals.Add(new Tuple<Vector3, Vector2, Vector3>(
+                    new Vector3(faceData.Vertices[c * 3], faceData.Vertices[c * 3 + 1], faceData.Vertices[c * 3 + 2]), 
+                    new Vector2(faceData.TexCoords[a * 2], faceData.TexCoords[a * 2 + 1]),
+                    new Vector3(faceData.Normals[c * 3], faceData.Normals[c * 3 + 1], faceData.Normals[c * 3 + 2])
+                    ));
+
+                newVertsWithUvsAndNormals.Add(new Tuple<Vector3, Vector2, Vector3>(
+                    new Vector3(faceData.Vertices[b * 3], faceData.Vertices[b * 3 + 1], faceData.Vertices[b * 3 + 2]),
+                    new Vector2(faceData.TexCoords[b * 2], faceData.TexCoords[b * 2 + 1]),
+                    new Vector3(faceData.Normals[b * 3], faceData.Normals[b * 3 + 1], faceData.Normals[b * 3 + 2])
+                    ));
+
+                newVertsWithUvsAndNormals.Add(new Tuple<Vector3, Vector2, Vector3>(
+                    new Vector3(faceData.Vertices[a * 3], faceData.Vertices[a * 3 + 1], faceData.Vertices[a * 3 + 2]),
+                    new Vector2(faceData.TexCoords[c * 2], faceData.TexCoords[c * 2 + 1]),
+                    new Vector3(faceData.Normals[a * 3], faceData.Normals[a * 3 + 1], faceData.Normals[a * 3 + 2])
+                    ));
             }
+
+            //reindex
+            Dictionary<Tuple<Vector3, Vector2, Vector3>, ushort> indexedFaces 
+                = new Dictionary<Tuple<Vector3, Vector2, Vector3>, ushort>();
+
+            List<ushort> newIndexes = new List<ushort>();
+            List<Tuple<Vector3, Vector2, Vector3>> deduplicatedVertices = new List<Tuple<Vector3, Vector2, Vector3>>();
+
+            for (int i = 0; i < newVertsWithUvsAndNormals.Count; i++)
+            {
+                var vert = newVertsWithUvsAndNormals[i];
+
+                //see if we have this vert indexed already
+                ushort existingIndex;
+                if (indexedFaces.TryGetValue(vert, out existingIndex))
+                {
+                    //yes, add the existing index to the new index list
+                    newIndexes.Add(existingIndex);
+                }
+                else
+                {
+                    //no, we need to add this vertex and index
+                    var idxPos = deduplicatedVertices.Count;
+                    indexedFaces.Add(vert, (ushort)idxPos);
+                    deduplicatedVertices.Add(vert);
+                    newIndexes.Add((ushort)(indexBase + idxPos));
+                }
+            }
+
+            //dump the new vertices, normals and UVs
+            Vertices.AddRange(deduplicatedVertices.SelectMany(v => new [] { v.Item1.X, v.Item1.Y, v.Item1.Z }));
+            Normals.AddRange(deduplicatedVertices.SelectMany(v => new[] { v.Item3.X, v.Item3.Y, v.Item3.Z }));
+            UVs.AddRange(deduplicatedVertices.SelectMany(v => new[] { v.Item2.X, v.Item2.Y}));
+
+            Indices.AddRange(newIndexes);
+
+            //dump a material for the entire VIEWER FACE
+            Materials.Add(face.TextureFace);
 
             SubMeshes.Add(
                 new SubmeshDesc
