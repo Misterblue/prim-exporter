c63018d6f7181799cbfd7ed819049bf501d3994e
David Daeschler <david.daeschler@gmail.com>
2016-10-28 15:43:39 -0500

    Complete retriangulation to minimize draw work The new code should only cause separate submeshes to be created for new materials regardless of their position in the face list, as well as remove duplicated vertices in the case where two faces that are connected share the exact same vertices

--- InWorldz.PrimExporter/InWorldz.PrimExporter.ExpLib/ImportExport/BabylonJSONPrimFaceCombiner.cs
+++ InWorldz.PrimExporter/InWorldz.PrimExporter.ExpLib/ImportExport/BabylonJSONPrimFaceCombiner.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using OpenMetaverse;
+using OpenMetaverse.Rendering;
 
 namespace InWorldz.PrimExporter.ExpLib.ImportExport
 {
@@ -21,83 +24,129 @@ namespace InWorldz.PrimExporter.ExpLib.ImportExport
         public List<float> Vertices = new List<float>();
         public List<float> Normals = new List<float>();
         public List<float> UVs = new List<float>();
-        public List<OpenMetaverse.Primitive.TextureEntryFace> Materials = new List<OpenMetaverse.Primitive.TextureEntryFace>();
+        public List<Primitive.TextureEntryFace> Materials = new List<Primitive.TextureEntryFace>();
         public List<SubmeshDesc> SubMeshes = new List<SubmeshDesc>();
-        
-        public void CombineFace(OpenMetaverse.Rendering.Face face)
+
+        private SortedList<int, Face> _sortedFaces = new SortedList<int, Face>();
+        private Dictionary<ulong, int> _knownMaterials = new Dictionary<ulong, int>();
+
+        public void CombineFace(Face face)
         {
-            int verticesBase = Vertices.Count;
-            int materialBase = Materials.Count;
-            int indexBase = Indices.Count;
-
-            PrimFace.FaceData faceData = (PrimFace.FaceData)face.UserData;
-
-            //negate X on the vertices and normals to account for a
-            //different coordinate space
-            for (int i = 0; i < faceData.Vertices.Length; i++)
-            {
-                if (i % 3 == 0)
-                {
-                    Vertices.Add(-faceData.Vertices[i]);
-                }
-                else
-                {
-                    Vertices.Add(faceData.Vertices[i]);
-                }
-            }
-
-            for (int i = 0; i < faceData.Normals.Length; i++)
-            {
-                if (i % 3 == 0)
-                {
-                    Normals.Add(-faceData.Normals[i]);
-                }
-                else
-                {
-                    Normals.Add(faceData.Normals[i]);
-                }
-            }
-            
-            UVs.AddRange(faceData.TexCoords);
-                
-            for (int i = 0; i < faceData.Indices.Length; i += 3)
-            {
-                ushort a = (ushort)(faceData.Indices[i] + (verticesBase / 3));
-                ushort b = (ushort)(faceData.Indices[i + 1] + (verticesBase / 3));
-                ushort c = (ushort)(faceData.Indices[i + 2] + (verticesBase / 3));
-
-                Indices.Add(a);
-                Indices.Add(b);
-                Indices.Add(c);
-            }
-
-            //if this submesh and the last are using identical materials, 
-            //combine them into a single face
             ulong matHash = _objHasher.GetMaterialFaceHash(face.TextureFace);
-            var lastSubMesh = SubMeshes.LastOrDefault();
-            if (lastSubMesh != null && lastSubMesh.MaterialHash == matHash)
-            {
-                //combine.. just add to the vertex/index counts
-                lastSubMesh.VerticesCount += Vertices.Count - verticesBase;
-                lastSubMesh.IndexCount += Indices.Count - indexBase;
-            }
-            else
-            {
-                //dump a material for the entire VIEWER FACE
-                Materials.Add(face.TextureFace);
-
-                SubMeshes.Add(
-                    new SubmeshDesc
-                    {
-                        MaterialHash = matHash,
-                        MaterialIndex = materialBase,
-                        VerticesStart = verticesBase,
-                        VerticesCount = Vertices.Count - verticesBase,
-                        IndexStart = indexBase,
-                        IndexCount = Indices.Count - indexBase
-                    });
-            }
             
+            int matIdx;
+            if (! _knownMaterials.TryGetValue(matHash, out matIdx))
+            {
+                _knownMaterials.Add(matHash, Materials.Count);
+                matIdx = Materials.Count;
+
+                Materials.Add(face.TextureFace);
+            }
+
+            _sortedFaces.Add(matIdx, face);
+        }
+
+        public void Complete()
+        {
+            List<Tuple<Vector3, Vector2, Vector3, int>> newVertsWithUvsAndNormalsAndMaterial =
+                    new List<Tuple<Vector3, Vector2, Vector3, int>>();
+
+            CreateVertexCloud(newVertsWithUvsAndNormalsAndMaterial);
+
+            //reindex
+            Dictionary<Tuple<Vector3, Vector2, Vector3, int>, ushort> indexedFaces
+                = new Dictionary<Tuple<Vector3, Vector2, Vector3, int>, ushort>();
+
+            List<ushort> newIndexes = new List<ushort>();
+            List<Tuple<Vector3, Vector2, Vector3, int>> deduplicatedVertices 
+                = new List<Tuple<Vector3, Vector2, Vector3, int>>();
+
+            int lastMat = newVertsWithUvsAndNormalsAndMaterial[0].Item4;
+            int vertStart = 0;
+            int indexStart = 0;
+            for (int i = 0; i < newVertsWithUvsAndNormalsAndMaterial.Count; i++)
+            {
+                var vert = newVertsWithUvsAndNormalsAndMaterial[i];
+
+                if (lastMat != vert.Item4)
+                {   
+                    //material has changed, create and add a new submesh here
+                    SubMeshes.Add(
+                        new SubmeshDesc
+                        {
+                            MaterialIndex = lastMat,
+                            VerticesStart = vertStart,
+                            VerticesCount = deduplicatedVertices.Count - vertStart,
+                            IndexStart = indexStart,
+                            IndexCount = newIndexes.Count - indexStart
+                        });
+
+                    lastMat = vert.Item4;
+                    vertStart = deduplicatedVertices.Count;
+                    indexStart = newIndexes.Count;
+                }
+
+                //see if we have this vert indexed already
+                ushort existingIndex;
+                if (indexedFaces.TryGetValue(vert, out existingIndex))
+                {
+                    //yes, add the existing index to the new index list
+                    newIndexes.Add(existingIndex);
+                }
+                else
+                {
+                    //no, we need to add this vertex and index
+                    var idxPos = deduplicatedVertices.Count;
+                    indexedFaces.Add(vert, (ushort)idxPos);
+                    deduplicatedVertices.Add(vert);
+                    newIndexes.Add((ushort)idxPos);
+                }
+            }
+
+            //dump the new vertices, normals and UVs
+            Vertices.AddRange(deduplicatedVertices.SelectMany(v => new[] { v.Item1.X, v.Item1.Y, v.Item1.Z }));
+            Normals.AddRange(deduplicatedVertices.SelectMany(v => new[] { v.Item3.X, v.Item3.Y, v.Item3.Z }));
+            UVs.AddRange(deduplicatedVertices.SelectMany(v => new[] { v.Item2.X, v.Item2.Y }));
+
+            Indices.AddRange(newIndexes);
+        }
+
+        private void CreateVertexCloud(List<Tuple<Vector3, Vector2, Vector3, int>> newVertsWithUvsAndNormalsAndMaterial)
+        {
+            foreach (var matAndface in _sortedFaces)
+            {
+                int verticesBase = Vertices.Count;
+
+                PrimFace.FaceData faceData = (PrimFace.FaceData) matAndface.Value.UserData;
+
+                for (int i = 0; i < faceData.Indices.Length; i += 3)
+                {
+                    ushort a = (ushort) (faceData.Indices[i] + (verticesBase/3));
+                    ushort b = (ushort) (faceData.Indices[i + 1] + (verticesBase/3));
+                    ushort c = (ushort) (faceData.Indices[i + 2] + (verticesBase/3));
+
+                    newVertsWithUvsAndNormalsAndMaterial.Add(new Tuple<Vector3, Vector2, Vector3, int>(
+                        new Vector3(-faceData.Vertices[a*3], faceData.Vertices[a*3 + 1], faceData.Vertices[a*3 + 2]),
+                        new Vector2(faceData.TexCoords[a*2], faceData.TexCoords[a*2 + 1]),
+                        new Vector3(-faceData.Normals[a*3], faceData.Normals[a*3 + 1], faceData.Normals[a*3 + 2]),
+                        matAndface.Key
+                        ));
+
+                    newVertsWithUvsAndNormalsAndMaterial.Add(new Tuple<Vector3, Vector2, Vector3, int>(
+                        new Vector3(-faceData.Vertices[b*3], faceData.Vertices[b*3 + 1], faceData.Vertices[b*3 + 2]),
+                        new Vector2(faceData.TexCoords[b*2], faceData.TexCoords[b*2 + 1]),
+                        new Vector3(-faceData.Normals[b*3], faceData.Normals[b*3 + 1], faceData.Normals[b*3 + 2]),
+                        matAndface.Key
+                        ));
+
+                    newVertsWithUvsAndNormalsAndMaterial.Add(new Tuple<Vector3, Vector2, Vector3, int>(
+                        new Vector3(-faceData.Vertices[c*3], faceData.Vertices[c*3 + 1], faceData.Vertices[c*3 + 2]),
+                        new Vector2(faceData.TexCoords[c*2], faceData.TexCoords[c*2 + 1]),
+                        new Vector3(-faceData.Normals[c*3], faceData.Normals[c*3 + 1], faceData.Normals[c*3 + 2]),
+                        matAndface.Key
+                        ));
+                }
+            }
         }
     }
 }
