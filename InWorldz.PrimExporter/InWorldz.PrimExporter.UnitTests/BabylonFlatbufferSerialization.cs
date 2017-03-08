46e8bb7bb9d4410b18c944969b15826eff44419c
David Daeschler <david.daeschler@gmail.com>
2017-03-08 13:00:35 -0600

    Start working on unit tests

--- InWorldz.PrimExporter/InWorldz.PrimExporter.UnitTests/BabylonFlatbufferSerialization.cs
+++ InWorldz.PrimExporter/InWorldz.PrimExporter.UnitTests/BabylonFlatbufferSerialization.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace InWorldz.PrimExporter.UnitTests
 {
+    [TestFixture]
     public class BabylonFlatbufferSerialization
     {
-
+        [Test]
+        public void TestSerializeDeserialize()
+        {
+            
+        }
     }
 }
